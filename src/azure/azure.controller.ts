263229a661d3041d51518ca8771529b08645d3e8
swapnil shinde <swapnilshinde9382@gmail.com>
2025-04-04 02:09:19 +0800

    restrict file size to 1 mb

--- src/azure/azure.controller.ts
+++ src/azure/azure.controller.ts
@@ -1,47 +1,59 @@
 // Create a file: src/file/file.controller.ts
-import { Body, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Express } from 'express';
 import { AzureStorageService } from './azure.service';
 import { FileUploadDto } from './dto/azure.dto';
-import { ApiBody, ApiConsumes } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiConsumes, ApiSecurity } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { CurrentUser } from 'src/decorators';
 
 @Controller('file')
 export class FileController {
-  constructor(private readonly azureStorageService: AzureStorageService) {}
+    constructor(private readonly azureStorageService: AzureStorageService) { }
 
-  @Post('upload')
-  @UseInterceptors(FileInterceptor('file'))
-  @ApiConsumes('multipart/form-data')
-  @ApiBody({
-    schema: {
-      type: 'object',
-      properties: {
-        file: { type: 'string', format: 'binary' },
-        containerName: { type: 'string' },
-      },
-    },
-  }) 
-  async uploadFile(@UploadedFile() file: any, @Body() fileUploadDto) {
-    try {
-      // Generate a unique name for the blob
-      const blobName = `${Date.now()}-${file.originalname}`;
-      
-      // Upload to the 'files' container
-      const url = await this.azureStorageService.upload(fileUploadDto.containerName!, blobName, file);
-      
-      return {
-        message: 'File uploaded successfully',
-        url,
-        fileName: file.originalname,
-        contentType: file.mimetype,
-        size: file.size,
-      };
-    } catch (error) {
-      return {
-        message: 'Failed to upload file',
-        error: error.message,
-      };
+    @Post('upload')
+    @UseInterceptors(FileInterceptor('file'))
+    @UseGuards(JwtAuthGuard)
+    @ApiBearerAuth()
+    @ApiSecurity('bearer')
+    @ApiConsumes('multipart/form-data')
+    @ApiBody({
+        schema: {
+            type: 'object',
+            properties: {
+                file: { type: 'string', format: 'binary' },
+                containerName: { type: 'string' },
+            },
+        },
+    })
+    async uploadFile(@UploadedFile() file: any, @Body() fileUploadDto, @CurrentUser() user) {
+        try {
+
+            const MAX_FILE_SIZE =  1 * 1024 * 1024;
+            if (file.size > MAX_FILE_SIZE) {
+                throw new BadRequestException(`File size exceeds the maximum limit of 1MB. Current size: ${(file.size / (1024 * 1024)).toFixed(2)}MB`);
+                return;
+            }
+
+            // Generate a unique name for the blob
+            const blobName = `${user.userId}-${Date.now()}-${file.originalname}`;
+
+            // Upload to the 'files' container
+            const url = await this.azureStorageService.upload(fileUploadDto.containerName!, blobName, file);
+
+            return {
+                message: 'File uploaded successfully',
+                url,
+                fileName: file.originalname,
+                contentType: file.mimetype,
+                size: file.size,
+            };
+        } catch (error) {
+            return {
+                message: 'Failed to upload file',
+                error: error.message,
+            };
+        }
     }
-  }
 }
