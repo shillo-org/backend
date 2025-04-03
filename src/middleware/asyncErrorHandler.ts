7addd805674b9291f4a6480adc0023415cc78724
swapnil shinde <swapnilshinde9382@gmail.com>
2025-04-04 00:39:59 +0800

    migrate to nest js

--- src/middleware/asyncErrorHandler.ts
+++
@@ -1,7 +0,0 @@
-import e, { Request, Response, NextFunction } from "express";
-
-export const asyncErrorHandler = (fn: Function) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
-  };
-};
