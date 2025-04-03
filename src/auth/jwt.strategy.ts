39db6f4ae4177e149ffba5cea9f3fe9ca11a5556
swapnil shinde <swapnilshinde9382@gmail.com>
2025-04-04 01:44:17 +0800

    add default secret

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -11,7 +11,7 @@ export class JWTStrategy extends PassportStrategy(Strategy) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
-            secretOrKey: configService.get<string>('JWT_SECRET')!
+            secretOrKey: configService.get<string>('JWT_SECRET',"supersecret")!
         });
     }
 
