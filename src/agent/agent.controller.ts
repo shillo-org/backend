61ad4c35be3b926bc75c21ed9762c13bf26b9662
Ankit Kokane <ankitkokane90@gmail.com>
2025-04-05 12:53:44 +0530

    minor fix

--- src/agent/agent.controller.ts
+++ src/agent/agent.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Get, NotFoundException, Param, ParseIntPipe, Post, Query, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiBearerAuth, ApiSecurity } from '@nestjs/swagger';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AiTokenDto } from './dto/post/aiToken.dto';
@@ -12,95 +22,101 @@ import { PrivyAuthGuard } from 'src/auth/privy-auth-guard';
 
 @Controller('agent')
 export class AgentController {
+  constructor(
+    private agentSerivce: AgentService,
+    private prismaService: PrismaService,
+  ) {}
 
-    constructor(private agentSerivce: AgentService, private prismaService: PrismaService) {}
+  async checkUserOwnsAgent(agentTokenId: number, userId: number) {
+    const token = await this.prismaService.aIToken.findFirst({
+      where: {
+        id: agentTokenId,
+        userId: userId,
+      },
+    });
 
-    async checkUserOwnsAgent(agentTokenId: number, userId: number) { 
-
-        const token = await this.prismaService.aIToken.findFirst({
-            where: {
-                id: agentTokenId,
-                userId: userId
-            }
-        })
-
-        if (token === null) {
-            throw new NotFoundException("Token Not found");
-        }
-
+    if (token === null) {
+      throw new NotFoundException('Token Not found');
     }
+  }
 
-    @Post("create-token")
-    @UseGuards(PrivyAuthGuard)
-    @ApiBearerAuth()
-    @ApiSecurity('bearer')
-    async createToken(@Body() aiTokenDto: AiTokenDto, @GetUser user: any) {
+  @Post('create-token')
+  @UseGuards(PrivyAuthGuard)
+  @ApiBearerAuth()
+  @ApiSecurity('bearer')
+  async createToken(@Body() aiTokenDto: AiTokenDto, @GetUser user: any) {
+    console.log(user);
+    return this.agentSerivce.createToken(
+      user.wallet.address as string,
+      aiTokenDto.tokenName,
+      aiTokenDto.symbol,
+      aiTokenDto.tokenDescription,
+      aiTokenDto.tokenImageUrl,
+      aiTokenDto.supply,
+      aiTokenDto.contractAddress,
+      aiTokenDto.website,
+      aiTokenDto.twitter,
+      aiTokenDto.telegram,
+      aiTokenDto.discord,
+      aiTokenDto.youtube,
+      aiTokenDto.youtubeChannelId,
+      aiTokenDto.twitchChannelId,
+    );
+  }
 
-        return this.agentSerivce.createToken(
-            user.userId as number,
-            aiTokenDto.tokenName,
-            aiTokenDto.symbol,
-            aiTokenDto.tokenDescription,
-            aiTokenDto.tokenImageUrl,
-            aiTokenDto.supply,
-            aiTokenDto.contractAddress,
-            aiTokenDto.website,
-            aiTokenDto.twitter,
-            aiTokenDto.telegram,
-            aiTokenDto.discord,
-            aiTokenDto.youtube,
-            aiTokenDto.youtubeChannelId,
-            aiTokenDto.twitchChannelId
-        )
+  @Post('add-token-character')
+  @UseGuards(PrivyAuthGuard)
+  @ApiBearerAuth()
+  @ApiSecurity('bearer')
+  async addAgentCharacter(
+    @Body() agentCharacterDto: AgentCharacterDto,
+    @GetUser user,
+  ) {
+    await this.checkUserOwnsAgent(
+      agentCharacterDto.aiTokenId,
+      user.userId as number,
+    );
 
-    }
+    return this.agentSerivce.addAgentCharacter(
+      agentCharacterDto.aiTokenId,
+      agentCharacterDto.agentName,
+      agentCharacterDto.agentIpfsUrl,
+      agentCharacterDto.agentImageUrl,
+    );
+  }
 
-    @Post("add-token-character")
-    @UseGuards(PrivyAuthGuard)
-    @ApiBearerAuth()
-    @ApiSecurity('bearer')
-    async addAgentCharacter(@Body() agentCharacterDto: AgentCharacterDto, @GetUser user) {
+  @Post('add-token-personality')
+  @UseGuards(PrivyAuthGuard)
+  @ApiBearerAuth()
+  @ApiSecurity('bearer')
+  async addAgentPersonality(
+    @Body() agentPersonalityDto: AgentPersonalityDto,
+    @GetUser user,
+  ) {
+    await this.checkUserOwnsAgent(
+      agentPersonalityDto.aiTokenId,
+      user.userId as number,
+    );
 
-        await this.checkUserOwnsAgent(agentCharacterDto.aiTokenId, user.userId as number)
-        
-        return this.agentSerivce.addAgentCharacter(
-            agentCharacterDto.aiTokenId,
-            agentCharacterDto.agentName,
-            agentCharacterDto.agentIpfsUrl,
-            agentCharacterDto.agentImageUrl
-        )
+    return this.agentSerivce.addPersonality(
+      agentPersonalityDto.aiTokenId,
+      agentPersonalityDto.voiceType,
+      agentPersonalityDto.personalityType,
+    );
+  }
 
-    }
+  @Get('')
+  async getAgents(@Query() filters: GetAgentsDto) {
+    return this.agentSerivce.getAgents(filters);
+  }
 
-    @Post("add-token-personality")
-    @UseGuards(PrivyAuthGuard)
-    @ApiBearerAuth()
-    @ApiSecurity('bearer')
-    async addAgentPersonality(@Body() agentPersonalityDto: AgentPersonalityDto, @GetUser user) {
+  @Get('templates')
+  async getAgentTemplates() {
+    return this.agentSerivce.getAgentTemplates();
+  }
 
-        await this.checkUserOwnsAgent(agentPersonalityDto.aiTokenId, user.userId as number)
-
-        return this.agentSerivce.addPersonality(
-            agentPersonalityDto.aiTokenId,
-            agentPersonalityDto.voiceType,
-            agentPersonalityDto.personalityType
-        )
-
-    }
-
-    @Get("")
-    async getAgents(@Query() filters: GetAgentsDto) {
-        return this.agentSerivce.getAgents(filters)
-    }
-
-    @Get("templates")
-    async getAgentTemplates() {
-        return this.agentSerivce.getAgentTemplates()
-    }
-
-    @Get(":id")
-    async getAgent(@Param("id", ParseIntPipe) id: number) {
-        return this.agentSerivce.getAgent(id)
-    }
-
+  @Get(':id')
+  async getAgent(@Param('id', ParseIntPipe) id: number) {
+    return this.agentSerivce.getAgent(id);
+  }
 }
--- src/agent/agent.service.ts
+++ src/agent/agent.service.ts
@@ -1,149 +1,175 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { GetAgentsDto } from './dto/get/agents.dto';
 
 @Injectable()
 export class AgentService {
+  constructor(private prismaService: PrismaService) {}
 
-    constructor(private prismaService: PrismaService) { }
+  async checkTokenExists(aiTokenId: number) {
+    const token = await this.prismaService.aIToken.findUnique({
+      where: {
+        id: aiTokenId,
+      },
+    });
 
-    async checkTokenExists(aiTokenId: number) {
+    if (!token) {
+      throw new NotFoundException('Token not found!');
+    }
+  }
 
-        const token = await this.prismaService.aIToken.findUnique({
-            where: {
-                id: aiTokenId
-            }
-        });
-
-        if (!token) {
-            throw new NotFoundException("Token not found!")
-        }
-
+  async createToken(
+    walletAddress: string,
+    tokenName: string,
+    symbol: string,
+    tokenDescription: string,
+    tokenImageUrl: string,
+    supply: number,
+    contractAddress: string,
+    website: string,
+    twitter: string,
+    telegram: string,
+    discord: string,
+    youtube: string,
+    youtubeChannelId: string,
+    twitchChannelId: string,
+  ) {
+    console.log(walletAddress);
+    const user = await this.prismaService.user.findFirst({
+      where: {
+        walletAddress: walletAddress,
+      },
+    });
+    console.log(user);
+    if (!user) {
+      throw new NotFoundException('User not found!');
     }
 
-    async createToken(userId: number, tokenName: string, symbol: string, tokenDescription: string, tokenImageUrl: string, supply: number, contractAddress: string, website: string, twitter: string, telegram: string, discord: string, youtube: string, youtubeChannelId: string, twitchChannelId: string) {
+    const agentCharacter = await this.prismaService.aIToken.create({
+      data: {
+        tokenName,
+        symbol,
+        tokenDescription,
+        tokenImageUrl,
+        supply,
+        contractAddress,
+        website,
+        twitter,
+        telegram,
+        discord,
+        youtube,
+        youtubeChannelId,
+        twitchChannelId,
+        userId: user.id,
+      },
+    });
 
-        const agentCharacter = await this.prismaService.aIToken.create({
-            data: {
-                tokenName,
-                symbol,
-                tokenDescription,
-                tokenImageUrl,
-                supply,
-                contractAddress,
-                website,
-                twitter,
-                telegram,
-                discord,
-                youtube,
-                userId,
-                youtubeChannelId,
-                twitchChannelId
-            }
-        })
-        return agentCharacter
+    return agentCharacter;
+  }
 
+  async addAgentCharacter(
+    aiTokenId: number,
+    agentName: string,
+    agentIpfsUrl: string,
+    agentImageUrl: string,
+  ) {
+    await this.checkTokenExists(aiTokenId);
+
+    try {
+      const agentCharacter = await this.prismaService.agentDisplay.create({
+        data: {
+          agentImageUrl,
+          agentIpfsUrl,
+          agentName,
+          aiTokenId,
+        },
+      });
+      return agentCharacter;
+    } catch {
+      throw new BadRequestException('Agent info already exists');
     }
+  }
 
-    async addAgentCharacter(aiTokenId: number, agentName: string, agentIpfsUrl: string, agentImageUrl: string) {
+  async addPersonality(
+    aiTokenId: number,
+    voiceType: string,
+    personalityType: string[],
+  ) {
+    await this.checkTokenExists(aiTokenId);
 
-        await this.checkTokenExists(aiTokenId);
+    const agentPersonality = await this.prismaService.aIToken.update({
+      where: { id: aiTokenId },
+      data: {
+        voiceType,
+        personalityType,
+      },
+    });
+    return agentPersonality;
+  }
 
-        try {
-            const agentCharacter = await this.prismaService.agentDisplay.create({
-                data: {
-                    agentImageUrl,
-                    agentIpfsUrl,
-                    agentName,
-                    aiTokenId
-                }
-            })
-            return agentCharacter
-        } catch {
-            throw new BadRequestException("Agent info already exists");
-        }
+  // Get API [multiple responses]
+  async getAgents(filters: GetAgentsDto) {
+    const skip = ((filters.page || 0) - 1) * (filters.pageSize || 0);
 
-    }
+    const results = await this.prismaService.aIToken.findMany({
+      where: {
+        ...(filters.walletAddress && filters.walletAddress !== ''
+          ? { user: { walletAddress: filters.walletAddress } }
+          : {}),
+        tokenName: filters.tokenName
+          ? { contains: filters.tokenName, mode: 'insensitive' }
+          : undefined,
+        tokenDescription: filters.tokenDescription
+          ? { contains: filters.tokenDescription, mode: 'insensitive' }
+          : undefined,
+      },
+      skip: skip,
+      take: filters.pageSize || 0,
+      select: {
+        id: true,
+        tokenName: true,
+        symbol: true,
+        tokenDescription: true,
+        tokenImageUrl: true,
+        contractAddress: true,
+        personalityType: true,
+      },
+      orderBy: {
+        createdAt: 'desc', // Order by latest created date
+      },
+    });
 
+    return results;
+  }
 
-    async addPersonality(aiTokenId: number, voiceType: string, personalityType: string[]) {
+  // Show agent detail
+  async getAgent(id: number) {
+    const token = await this.prismaService.aIToken.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        agentDisplay: true,
+        user: {
+          select: {
+            username: true,
+            walletAddress: true,
+            profile_pic: true,
+            createdAt: true,
+          },
+        },
+      },
+    });
 
-        await this.checkTokenExists(aiTokenId);
+    return token;
+  }
 
-        const agentPersonality = await this.prismaService.aIToken.update({
-            where: { id: aiTokenId },
-            data: {
-                voiceType,
-                personalityType
-            }
-        })
-        return agentPersonality
-
-    }
-
-
-    // Get API [multiple responses]
-    async getAgents(filters: GetAgentsDto) {
-
-        const skip = ((filters.page || 0 ) - 1) * (filters.pageSize || 0);
-
-        const results = await this.prismaService.aIToken.findMany({
-            where: {
-                ...(
-                    filters.walletAddress && filters.walletAddress !== ""
-                    ? {user: {walletAddress: filters.walletAddress}}
-                    : {}
-                ),
-                tokenName: filters.tokenName ? { contains: filters.tokenName, mode: 'insensitive' } : undefined,
-                tokenDescription: filters.tokenDescription ? { contains: filters.tokenDescription, mode: 'insensitive' } : undefined,
-            },
-            skip: skip,
-            take: filters.pageSize || 0,
-            select: {
-                id: true,
-                tokenName: true,
-                symbol: true,
-                tokenDescription: true,
-                tokenImageUrl: true,
-                contractAddress: true,
-                personalityType: true
-            },
-            orderBy: {
-                createdAt: 'desc', // Order by latest created date
-            },
-        });
-
-        return results;
-
-    }
-
-    // Show agent detail
-    async getAgent(id: number) {
-
-        const token = await this.prismaService.aIToken.findUnique({
-            where: {
-                id
-            }, 
-            include: {
-                agentDisplay: true,
-                user: {
-                    select: {
-                        username: true,
-                        walletAddress: true,
-                        profile_pic: true,
-                        createdAt: true
-                    }
-                }
-            }
-        });
-
-        return token
-
-    }
-
-    async getAgentTemplates() {
-        const templates = await this.prismaService.agentDisplayTemplate.findMany()
-        return templates
-    }
-
+  async getAgentTemplates() {
+    const templates = await this.prismaService.agentDisplayTemplate.findMany();
+    return templates;
+  }
 }
